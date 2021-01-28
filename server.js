defc45f91d0c8808654678650740fa4aaef51661
morellam-dev <4561733+morellam-dev@users.noreply.github.com>
2021-01-28 12:23:11 -0500

    Set live-server to ignore dotfiles

--- server.js
+++ server.js
@@ -4,5 +4,6 @@ const liveServer = require("live-server");
 const params = {
   file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
   wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
+  ignore: ".*"
 };
 liveServer.start(params);
