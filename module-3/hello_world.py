04f58f5958187acb5ba88444fcfd15ab485a38f0
morellam-dev <4561733+morellam-dev@users.noreply.github.com>
2021-04-06 12:08:45 -0400

    Updates

--- module-3/hello_world.py
+++
@@ -1,2 +0,0 @@
-# prints "hello world"
-print("hello world!")
--- server.js
+++
@@ -1,9 +0,0 @@
-// server.js
-const liveServer = require("live-server");
-
-const params = {
-  file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
-  wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
-  ignore: ".*"
-};
-liveServer.start(params);
